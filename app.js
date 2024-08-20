78e18fa9e9f639f9ef378ed930fd47aa51895518
João Gabriel Ferreira <ojoaogabrielf@gmail.com>
2024-08-20 18:27:48 -0300

    app.js

--- app.js
+++ app.js
@@ -63,5 +63,5 @@ function reiniciarJogo(){
     limparCampo();
     tentativas = 1;
     exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true)
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
